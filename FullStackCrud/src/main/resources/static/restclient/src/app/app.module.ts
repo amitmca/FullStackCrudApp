23f75adef3725370c0ecf6c548f768b3b63163ba
Nikhil Jaunjal <jnikhil26@gmail.com>
2018-02-03 22:51:15 +0530

    client setup is ready, routes are working

---
+++ FullStackCrud/src/main/resources/static/restclient/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { Route } from '@angular/compiler/src/core';
+
+const appRoutes: Routes = [
+  { path: '', component: ListUserComponent },
+  { path: 'op', component: UserFormComponent }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListUserComponent,
+    UserFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
