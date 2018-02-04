3f70bc9ef21705f146cdfa5e7252b4c5bf06cc4e
Nikhil Jaunjal <jnikhil26@gmail.com>
2018-02-04 11:21:13 +0530

    UI created with bootstrap

--- FullStackCrud/src/main/java/com/nikhil/controller/UserController.java
+++ FullStackCrud/src/main/java/com/nikhil/controller/UserController.java
@@ -3,6 +3,7 @@ package com.nikhil.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.nikhil.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class UserController
 {
 	@Autowired
@@ -39,7 +41,7 @@ public class UserController
 		userRepository.delete(id);
 		return true;
 	}
-
+	
 	@PostMapping("/user")
 	public User createUser(User user)
 	{
